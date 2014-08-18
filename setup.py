8030fc1726d00bd070dc7ae971fb91e0db5d2c1e
Miroslav Batchkarov <mbatchkarov@gmail.com>
2014-08-18 11:30:19 +0100

    Bumped version to 2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='critical_difference',
-    version='1.0',
+    version='2.0',
     packages=['critical_difference'],
     author=['Miroslav Batchkarov'],
     author_email=['mbatchkarov@gmail.com'],
