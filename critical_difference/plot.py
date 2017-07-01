48b1183cd57dc6d80890b6254fa1eeb881ab68b3
Miroslav Batchkarov <miroslav@skim.it>
2017-07-01 18:11:15 +0200

    pep8

--- critical_difference/plot.py
+++ critical_difference/plot.py
@@ -1,5 +1,6 @@
 from itertools import combinations
 from math import ceil
+
 import matplotlib.pylab as plt
 from matplotlib.backends.backend_agg import FigureCanvasAgg
 from matplotlib.lines import Line2D
@@ -19,54 +20,57 @@ def _contained_in_larger_interval(i, j, not_sig):
 
 def merge_nonsignificant_cliques(not_sig):
     # keep only longest
-    longest = [(i, j) for i, j in not_sig if not _contained_in_larger_interval(i, j, not_sig)]
+    longest = [(i, j) for i, j in not_sig if
+               not _contained_in_larger_interval(i, j, not_sig)]
     return longest
 
 
 def do_plot(x, get_linked_methods, names=None,
             arrow_vgap=.2, link_voffset=.15, link_vgap=.1,
             xlabel=None
-):
+            ):
     """
-    Draws a critical difference graph, which is used to display  the differences in methods'
-    performance. This is inspired by the plots used in:
+    Draws a critical difference graph, which is used to display  the
+    differences in methods' performance. This is inspired by the plots used in:
 
     See Janez Demsar, Statistical Comparisons of Classifiers over
     Multiple Data Sets, 7(Jan):1--30, 2006.
 
-    Methods are drawn on an axis and connected with a line if their performance is not significantly
-    different.
+    Methods are drawn on an axis and connected with a line if their
+    performance is not significantly different.
 
     Requires `matplotlib`
 
     :param x: List of average methods' scores.
     :type x: list-like
 
-    :param names: List of methods' names.
-    :param get_linked_methods: callable that returns a list of tuples of indices of all pairs of methods
-    that are not significantly different and should be connected in the diagram. Each tuple must be sorted,
-    and no duplicate tuples should be contained in the list.
+    :param names: List of methods' names. :param get_linked_methods: callable
+    that returns a list of tuples of indices of all pairs of methods that are
+    not significantly different and should be connected in the diagram. Each
+    tuple must be sorted, and no duplicate tuples should be contained in the
+    list.
 
         Examples:
          - [(0, 1), (3, 4), (4, 5)] is correct
          - [(0, 1), (3, 4), (4, 5), (3,4)] contains a duplicate
          - [(4, 3)] contains a non-sorted tuple
 
-    If there is a cluster of non-significant differences (e.g. 1=2, 2=3, 1=3), `graph_ranks` will draw
-    just a single link connecting all of them.
+    If there is a cluster of non-significant differences (e.g. 1=2, 2=3,
+    1=3), `graph_ranks` will draw just a single link connecting all of them.
 
-    Note: the indices returned by this callable should refer to positions in `scores` after it is sorted in increasing
-    order. It is to avoid confusion this function raises if `scores` is not sorted.
-    :param arrow_vgap: vertical space between the arrows that point to method names.  Scale is 0 to 1, fraction of axis
-    :param link_vgap: vertical space between the lines that connect methods that are not significantly different.
-     Scale is 0 to 1, fraction of axis size
-    :param link_voffset: offset from the axis of the links that connect non-significant methods
+    Note: the indices returned by this callable should refer to positions in
+    `scores` after it is sorted in increasing order. It is to avoid confusion
+    this function raises if `scores` is not sorted. :param arrow_vgap:
+    vertical space between the arrows that point to method names.  Scale is 0
+    to 1, fraction of axis :param link_vgap: vertical space between the lines
+    that connect methods that are not significantly different. Scale is 0 to
+    1, fraction of axis size :param link_voffset: offset from the axis of the
+    links that connect non-significant methods
     """
     if names is None:
         names = list(range(len(x)))
 
-    # remove both axes and the frame
-    # http://www.shocksolution.com/2011/08/removing-an-axis-or-both-axes-from-a-matplotlib-plot/
+    # remove both axes and the frame: http://bit.ly/2tBIlWv
     fig, ax = plt.subplots(1, 1, figsize=(6, 2), subplot_kw=dict(frameon=False))
     ax.get_xaxis().tick_bottom()
     ax.get_yaxis().set_visible(False)
@@ -82,7 +86,8 @@ def do_plot(x, get_linked_methods, names=None,
     # this must be done after plotting
     xmin, xmax = ax.get_xaxis().get_view_interval()
     ymin, ymax = ax.get_yaxis().get_view_interval()
-    ax.add_artist(Line2D((xmin, xmax), (ymin, ymin), color='black', linewidth=2))
+    ax.add_artist(
+        Line2D((xmin, xmax), (ymin, ymin), color='black', linewidth=2))
 
     # add an optional label to the x axis
     if xlabel:
@@ -106,7 +111,8 @@ def do_plot(x, get_linked_methods, names=None,
 
     # draw horizontal lines linking non-significant methods
     linked_methods = merge_nonsignificant_cliques(get_linked_methods())
-    used_endpoints = set()  # where do the existing lines begin and end, (X, Y) coords
+    # where do the existing lines begin and end, (X, Y) coords
+    used_endpoints = set()
     y = link_voffset
     dy = link_vgap
 
@@ -147,18 +153,20 @@ def get_close_pairs(scores, threshold=1):
     n_methods = len(scores)
     allpairs = list(combinations(range(n_methods), 2))
 
-    not_sig = [(i, j) for i, j in allpairs if abs(scores[i] - scores[j]) <= threshold]
+    not_sig = [(i, j) for i, j in allpairs if
+               abs(scores[i] - scores[j]) <= threshold]
     return not_sig
 
 
-def my_get_lines(*args):
-    return [(0, 1), (1, 2), (2, 3),  # these three should be drawn on 2 level
-            (3, 4), (3, 5), (4, 5),  # this is a clique, just one line should be drawn
-    ]
-
-
 if __name__ == "__main__":
+    def dummy_significance_test(*args):
+        return [
+            (0, 1), (1, 2), (2, 3),  # these three should be drawn on 2 level
+            (3, 4), (3, 5), (4, 5),  # this is a clique, just one line
+        ]
+
+
     scores = sorted([31.43, 20.00, 28.93, 19.64, 25, 33.4])
     names = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth']
-    fig = do_plot(scores, my_get_lines, names, xlabel='accuracy, %')
+    fig = do_plot(scores, dummy_significance_test, names, xlabel='accuracy, %')
     print_figure(fig, "test.png", format='png')
